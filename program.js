9c5506b15217830ba51e6c77958c403e6c8381db
Lee <leewhite128@gmail.com>
2016-01-05 13:48:44 -0800

    ex. 12

--- program.js
+++ program.js
@@ -1,12 +1,12 @@
 var http = require('http');
-var fs = require('fs');
-
-var fileRead = process.argv[3];
+var map = require('through2-map');
 var port = Number(process.argv[2]);
 
 var server = http.createServer(function(req, res) {
-	var fsStream = fs.createReadStream(fileRead);
-	fsStream.pipe(res);
+	if (req.method != 'POST') return res.end('Not a POST request.\n');
+	req.pipe(map(function(chunk){
+		return chunk.toString().toUpperCase();
+	})).pipe(res);
 });
 
 server.listen(port);
